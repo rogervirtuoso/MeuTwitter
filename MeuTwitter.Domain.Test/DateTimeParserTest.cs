24bda4c786244bf3bd43a6d82a08dc49bb60adde
roger virtuoso <rogervirtuoso@gmail.com>
2018-12-08 15:00:35 -0200

    Coverage increased

---
+++ MeuTwitter.Domain.Test/DateTimeParserTest.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace MeuTwitter.Domain.Test
+{
+    public class DateTimeParserTest
+    {
+        [Test]
+        public void Domain_Valid_Message_Post_Test()
+        {
+            //Arrange
+            var post = new Post();
+            post.SetMessage("Hello World", DateTime.Now.AddMinutes(-2));
+
+            //Action
+            var result = post.Validate();
+
+            //Assert
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public void Domain_Invalid_NullOrEmpty_Message_Post_Test()
+        {
+            //Arrange
+            var post = new Post();
+
+            //Action
+            var result = post.Validate();
+
+            //Assert
+            Assert.IsFalse(result);
+        }
+
+        [Test]
+        public void Domain_Invalid_Length_Message_Post_Test()
+        {
+            //Arrange
+            var post = new Post();
+            post.SetMessage(
+                "asheiuasheiuasheiuasheiuaheiuahiuehiuAHeiuahieuhasiuehaiusehaiusheiuasheiuahsasheiuasheiuasheiuasheiuaheiuahiuehiuAHeiuahieuhasiuehaiusehaiusheiuasheiuahs",
+                DateTime.Now.AddMinutes(-2));
+
+            //Action
+            var result = post.Validate();
+
+            //Assert
+            Assert.IsFalse(result);
+        }
+
+        [Test]
+        public void Domain_Invalid_Date_Post_Test()
+        {
+            //Arrange
+            var post = new Post();
+            post.SetMessage("Testando", DateTime.Now.AddMinutes(5));
+
+            //Action
+            var result = post.Validate();
+
+            //Assert
+            Assert.IsFalse(result);
+        }
+    }
+}
---
+++ src/MeuTwitter.Domain/Post.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MeuTwitter.Domain
+{
+    public class Post
+    {
+        public long Id { get; set; }
+        public string Message { get; private set; }
+        public DateTime PostDate { get; private set; }
+        public string DisplayPostDate { get; set; }
+
+        public void SetMessage(string message, DateTime postDate)
+        {
+            Message = message;
+            PostDate = postDate;
+        }
+
+        public bool Validate()
+        {
+            return !string.IsNullOrEmpty(Message)
+                   && Message.Length <= 140
+                   && PostDate < DateTime.Now;
+        }
+    }
+}
