d1d4cab9284afb8a166e3c23cb166c91682574bf
roger virtuoso <rogervirtuoso@gmail.com>
2018-12-08 13:56:55 -0200

    thirt test

--- src/MeuTwitter.Infra.Test/DateTimeParserTest.cs
+++ src/MeuTwitter.Infra.Test/DateTimeParserTest.cs
@@ -1,65 +1,126 @@
 ﻿using System;
-using System.Diagnostics;
 using NUnit.Framework;
 
 namespace MeuTwitter.Infra.Test
 {
     public class DateTimeParserTest
     {
-        [OneTimeSetUp]
-        public void OneTimeSetup()
-        {
-            Debug.WriteLine("Passou no ONETIMESETUP");
-        }
-
-        [SetUp]
-        public void Setup()
-        {
-            Debug.WriteLine("Passou no SETUP");
-        }
-
         [Test]
         public void Infra_OneMinute_Test()
         {
-            //Arrenge
-            var createdDate = DateTime.Now.AddMinutes(-1);
+            //Arrange
+            var dataCriacao = DateTime.Now.AddMinutes(-1);
 
             //Action
-            string result = DateTimeParser.TryParce(createdDate);
-            Debug.WriteLine("Passou no TEST");
-
+            var result = DateTimeParser.TryParse(dataCriacao);
 
             //Assert
-            Assert.AreEqual("1 minuto atrás", result);
-            Assert.IsTrue(true);
+            Assert.IsTrue(result.Contains("1 minuto atrás"));
         }
 
         [Test]
         public void Infra_TwoMinute_Test()
         {
-            //Arrenge
-            var createdDate = DateTime.Now.AddMinutes(-1);
+            //Arrange
+            var dataCriacao = DateTime.Now.AddMinutes(-2);
 
             //Action
-            string result = DateTimeParser.TryParce(createdDate);
-            Debug.WriteLine("Passou no TEST");
-
+            var result = DateTimeParser.TryParse(dataCriacao);
 
             //Assert
-            Assert.AreEqual("2 minuto atrás", result);
-            Assert.IsTrue(true);
+            Assert.IsTrue(result.Contains("2 minutos atrás"));
         }
 
-        [TearDown]
-        public void TearDown()
+        [Test]
+        public void Infra_OneHour_Test()
         {
-            Debug.WriteLine("Passou no TEARDOWN");
+            //Arrange
+            var dataCriacao = DateTime.Now.AddHours(-1);
+
+            //Action
+            var result = DateTimeParser.TryParse(dataCriacao);
+
+            //Assert
+            Assert.IsTrue(result.Contains("1 hora atrás"));
         }
 
-        [OneTimeTearDown]
-        public void OneTearDown()
+        [Test]
+        public void Infra_TwoHour_Test()
         {
-            Debug.WriteLine("Passou no ONETIMETEARDOWN");
+            //Arrange
+            var dataCriacao = DateTime.Now.AddHours(-2);
+
+            //Action
+            var result = DateTimeParser.TryParse(dataCriacao);
+
+            //Assert
+            Assert.IsTrue(result.Contains("2 horas atrás"));
+        }
+
+        [Test]
+        public void Infra_OneDay_Test()
+        {
+            //Arrange
+            var dataCriacao = DateTime.Now.AddDays(-1);
+
+            //Action
+            var result = DateTimeParser.TryParse(dataCriacao);
+
+            //Assert
+            Assert.IsTrue(result.Contains("1 dia atrás"));
+        }
+
+        [Test]
+        public void Infra_TwoDays_Test()
+        {
+            //Arrange
+            var dataCriacao = DateTime.Now.AddDays(-2);
+
+            //Action
+            var result = DateTimeParser.TryParse(dataCriacao);
+
+            //Assert
+            Assert.IsTrue(result.Contains("2 dias atrás"));
+        }
+
+        [Test]
+        public void Infra_TwoDaysFourHoursTenMinutes_Test()
+        {
+            //Arrange
+            var dataCriacao = DateTime.Now.AddDays(-2);
+            dataCriacao = dataCriacao.AddHours(-4);
+            dataCriacao = dataCriacao.AddMinutes(-10);
+
+            //Action
+            var result = DateTimeParser.TryParse(dataCriacao);
+
+            //Assert
+            Assert.IsTrue(result.Contains("2 dias, 4 horas, 10 minutos atrás"));
+        }
+
+        [Test]
+        public void Infra_OneDayOneHourOneMinute_Test()
+        {
+            //Arrange
+            var dataCriacao = DateTime.Now.AddDays(-1);
+            dataCriacao = dataCriacao.AddHours(-1);
+            dataCriacao = dataCriacao.AddMinutes(-1);
+
+            //Action
+            var result = DateTimeParser.TryParse(dataCriacao);
+
+            //Assert
+            Assert.IsTrue(result.Contains("1 dia, 1 hora, 1 minuto atrás"));
+        }
+
+        [Test]
+        public void Infra_InvalidDate_Test()
+        {
+            //Arrange
+            var dataCriacao = DateTime.Now.AddDays(2);
+
+            //Assert
+            Assert.Throws(typeof(ArgumentOutOfRangeException), () => DateTimeParser.TryParse(dataCriacao));
         }
     }
 }
--- src/MeuTwitter.Infra/DateTimeParser.cs
+++ src/MeuTwitter.Infra/DateTimeParser.cs
@@ -4,9 +4,59 @@ namespace MeuTwitter.Infra
 {
     public static class DateTimeParser
     {
-        public static string TryParce(DateTime createdDate)
+        public static string TryParse(DateTime dataCriacao)
         {
-            return "1 minuto atrás";
+            var mensagemDias = string.Empty;
+            var mensagemHoras = string.Empty;
+            var mensagemMinutos = string.Empty;
+            var dateTimeNow = DateTime.Now;
+
+            if (dataCriacao > dateTimeNow)
+                throw new ArgumentOutOfRangeException();
+
+            var diffTime = dateTimeNow - dataCriacao;
+
+            if (diffTime.Days > 0)
+            {
+                mensagemDias = diffTime.Days == 1 ? $"{diffTime.Days} dia" : $"{diffTime.Days} dias";
+            }
+
+            if (diffTime.Hours > 0)
+            {
+                mensagemHoras = diffTime.Hours == 1 ? $"{diffTime.Hours} hora" : $"{diffTime.Hours} horas";
+            }
+
+            if (diffTime.Minutes > 0)
+            {
+                mensagemMinutos = diffTime.Minutes == 1
+                    ? $"{diffTime.Minutes} minuto"
+                    : $"{diffTime.Minutes} minutos";
+            }
+
+            var mensagemFinal = string.Empty;
+
+            if (!string.IsNullOrWhiteSpace(mensagemDias))
+            {
+                mensagemFinal += mensagemDias;
+
+                if (!string.IsNullOrWhiteSpace(mensagemHoras) || !string.IsNullOrWhiteSpace(mensagemMinutos))
+                    mensagemFinal += ", ";
+            }
+
+            if (!string.IsNullOrWhiteSpace(mensagemHoras))
+            {
+                mensagemFinal += mensagemHoras;
+
+                if (!string.IsNullOrWhiteSpace(mensagemMinutos))
+                    mensagemFinal += ", ";
+            }
+
+            if (!string.IsNullOrWhiteSpace(mensagemMinutos))
+            {
+                mensagemFinal += mensagemMinutos;
+            }
+
+            return $"{mensagemFinal.Trim()} atrás";
         }
     }
 }
